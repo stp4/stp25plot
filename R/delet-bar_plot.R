91f620caf9c1725411ef46c81489aa266b99a953
stp4 <w.peter@statistik-peter.at>
2020-12-03 13:16:36 +0100

    nfg

---
+++ R/delet-bar_plot.R
@@ -0,0 +1,258 @@
+
+## Balken sind nicht Sinnvoll da das mit lattice einfacher geht##
+## bzw mit  panel.barchart.value 
+
+# bar_plot <- function(formula, data,
+#                     main="", xlab=NULL, ylab=NULL,
+# 
+#                     xlim=NULL, ylim=NULL,
+#                     col=NA,
+#                     percent=TRUE,
+#                     digits=2, #pos= 3,
+#                     suffix="",
+#                     type=1, #--Saeulendiagramm
+#                     groups=NULL,
+#                     margin = NULL,
+#                     value=TRUE,
+#                     fun=function(x) mean(x, na.rm=TRUE),
+#                     pos=NULL,
+#                     pos_panel= if(is.null(pos)) {
+#                         if(type==1 | type=="vertikal" ) 3
+#                         else 4
+#                     }else  pos ,
+#                     var_names= Formula_Names(formula, data),
+#                     panel=function(...)   {
+#                         panel.barchart(...)
+#                         if(value) panel.barchart.value(..., digits=digits, pos=pos_panel, suffix=suffix)
+#                     },
+#                     ...){
+# require(lattice)
+#     #  print(var_names)
+#     #  print(is_all_identical2(data[ var_names$yname]))
+#     if(!is_all_identical2(data[ var_names$yname])) {
+#         cat("\nDas Skalenniveau ist gemischt! und daher hann nichts berechnet werden\n")
+#         return(str(data[, var_names$yname]))
+#     }else{
+#         if(all(sapply(data[var_names$yname], is.factor))){
+# 
+#             #
+#             #print("Factor")
+#             # print(var_names$yname )
+#             if(!is.null(var_names$xname)) {
+#                 if(is.null(margin)) margin<-1
+#                 formula <- formula(paste( "~",
+#                                           paste(c(var_names$xname, var_names$yname) , collapse="+")
+# 
+#                 ))}
+# 
+#             xtb <- xtabs(formula, data)
+#             xdata <- data.frame( xtb )
+#             xnames <- names(xdata)[-ncol(xdata) ]
+#             prcdata <-   data.frame(prop.table(xtb, margin = margin)*100)
+#             n <- ncol(prcdata )
+#             x_data <- cbind(prcdata[-n], x__n= data.frame(xtb)[,n], x__Freq=prcdata[,n])
+#             #  print(x_data)
+#             vars  <- names(x_data)[1:(n-1)]
+#             xylim <- extendrange(c(0, x_data$x__Freq)) + c(0, 5)
+#             orgn  <- 0
+#             xylab <- "Prozent"
+#             suffix <- "%"
+# 
+#         }else{
+#             # print("gggg")
+#             m_data <- Recast2(formula, data, fun=fun)
+#             n_data <- Recast2(formula, data, fun=function(x) length(na.omit(x)))
+#             n <- ncol(m_data)
+#             names(m_data)[n-1] <- "x__variable"
+#             x_data <- cbind( m_data[-n], x__n=n_data[,n], x__Freq= m_data[,n])
+# 
+#             x_data<- x_data[c("x__variable", names(x_data)[-which(names(x_data) %in% "x__variable" )])]
+#             vars <- names(x_data)[1:(n-1)]
+#             rng <- range(data[var_names$yname], na.rm=TRUE)
+#             xylim <- extendrange(rng , f= 0.01) #    c(0,  x_data$x__Freq))
+#             orgn <- rng[1]
+#             xylab <- "Mittelwert"
+#         }
+#         if(length(vars)>1){
+# 
+#             groups_name <-  if(is.null(substitute(groups))) vars[2]
+#             else substitute(groups)
+#             # cat("\ngroups: ", groups_name, class(groups_name), "\n")
+#             if(groups_name=="F") warning("Achtung bei Gruppe mit Namen  F besteht verwechslungsgefahr besser FALSE verwenden")
+# 
+#             if(!is.logical(groups_name)) {
+#                 groups_name<- as.character(groups_name)
+# 
+#                 groups <- x_data[, groups_name]
+#                 vars <- vars[-which(vars %in% groups_name)]
+#             }
+#             else {
+#                 groups=NULL
+# 
+# 
+#             }
+#         }
+# 
+# 
+#         if(type==1 | type=="vertikal"){
+#             if(length(vars)==1) fm <- formula(paste( "x__Freq~", vars[1]))
+#             else if(length(vars)==2 & is.null(groups)) fm <- formula(paste( "x__Freq~", vars[2] ))
+#             else fm <- formula(paste("x__Freq~",vars[1], "|", paste(vars[-1], collapse="+")))
+#             if(is.null(ylim)) ylim <-xylim
+#             if(is.null(ylab))   ylab <- xylab
+#             print(fm)
+#             print(vars)
+#             p1 <- barchart(fm, x_data, groups=groups,
+#                            origin=orgn,
+#                            ylim= ylim, ylab=ylab, main=main,
+#                            panel=panel, ...)
+# 
+# 
+#         } else {
+#             if(length(vars)==1) fm <- formula(paste(vars[1], "~x__Freq"))
+#             else if(length(vars)==2 & is.null(groups)) fm <- formula(paste(vars[2] , "~x__Freq"))
+#             else fm <- formula(paste(vars[1],"~x__Freq", "|", paste(vars[-1], collapse="+")))
+#             if(is.null(xlim)) xlim <-xylim
+#             if(is.null(xlab))   xlab <- xylab
+# 
+#             p1 <- barchart(fm, x_data, groups=groups,
+#                            origin=orgn,
+#                            xlim= xlim, xlab=xlab, main=main,
+#                            panel=panel, ...)
+# 
+# 
+#         }
+#         print(p1)
+#         return(x_data)
+#     }
+# }
+# 
+# 
+# 
+# 
+# balken <- function(x,
+#                    data,
+#                    main = "",
+#                    ylab = "",
+#                    xlab = "",
+#                    cex.x = 1.5,
+#                    cex.y = 1.3,
+#                    cex.ltext = 1.1,
+#                    settings = set_lattice_ggplot(col.bar = cbPalette['blue']),
+#                    margin = 2,
+#                    include.value = FALSE,
+#                    auto.key = list(space = "bottom", columns = 2),
+#                    orientation = NULL,
+#                    wrap=NULL,
+#                    ...) {
+#     if(!is.null(wrap)) main <- wrap_sentence(main, wrap)
+#     
+#     n_vars <- length(all.vars(x))
+#     lhs <- all.vars(x)[1L]
+#     rhs <-  all.vars(x)[2L]
+#     chs <-  all.vars(x)[3L]
+#     
+#     if (n_vars == 1) {
+#         dat <- to_table(x, data)
+#         if (is.null(orientation))
+#             orientation <- 1
+#         
+#         
+#         if (orientation == 1) {
+#             fm <- formula(paste0(lhs, "~ Percent"))
+#             lattice::barchart(
+#                 fm,
+#                 dat,
+#                 main = main,
+#                 origin = 0,
+#                 xlab = list(label = xlab, cex = cex.x),
+#                 scales = list(y = list(cex = cex.y)),
+#                 par.settings = settings,
+#                 panel = function(x, y, ...) {
+#                     lattice::panel.barchart(x, y, ...)
+#                     if (include.value) {
+#                         prz <- stp25rndr::rndr_percent(dat$Percent, dat$Freq)
+#                         
+#                         ltext(
+#                             x = .4,
+#                             y = y,
+#                             adj = c(0, NA),
+#                             col = "white",
+#                             labels = prz,
+#                             cex = cex.ltext
+#                         )
+#                     }
+#                 },
+#                 ...
+#             )
+#         }
+#         else{
+#             fm <- formula(paste0("Percent ~ ", lhs))
+#             lattice::barchart(
+#                 fm,
+#                 dat,
+#                 main = main,
+#                 origin = 0,
+#                 ylab = list(label = ylab, cex = cex.y),
+#                 scales = list(x = list(cex = cex.x)),
+#                 par.settings = settings,
+#                 panel = function(x, y, ...) {
+#                     lattice::panel.barchart(x, y, ...)
+#                     if (include.value) {
+#                         prz <- stp25rndr::rndr_percent(dat$Percent, dat$Freq)
+#                         ltext(
+#                             x = x,
+#                             y = y - 5,
+#                             adj = c(NA, 0),
+#                             col = "white",
+#                             labels = prz,
+#                             cex = cex.ltext
+#                         )
+#                     }
+#                 },
+#                 ...
+#             )
+#         }
+#         
+#     }
+#     else if (n_vars == 2) {
+#         dat <- to_table(x, data, margin = margin)
+#         names(dat)[2] <- "groups"
+#         
+#         fm <- formula(paste("Percent ~ ", lhs))
+#         lattice::barchart(
+#             fm,
+#             dat,
+#             groups = groups,
+#             origin = 0,
+#             ylab = ylab,
+#             main = main,
+#             par.settings = settings,
+#             auto.key = auto.key,
+#             panel = function(x, y, ...) {
+#                 lattice::panel.barchart(x, y, ...)
+#                 if (include.value) {
+#                     prz <- stp25rndr::rndr_percent(dat$Percent, dat$Freq)
+#                     ltext(
+#                         x = x,
+#                         y =  y - 5,
+#                         adj = c(NA, 0),
+#                         col = "white",
+#                         labels = prz,
+#                         cex = cex.ltext
+#                     )
+#                 }
+#                 
+#             },
+#             ...
+#         )
+#         
+#     }
+#     else if (n_vars == 3) {
+#         list(n_vars, lhs, rhs, chs)
+#         
+#     }
+#     else{
+#         list(n_vars, NULL, NULL, NULL)
+#     }
+# }
---
+++ R/delet-plot.efflist.R
@@ -0,0 +1,80 @@
+#' Modifiziert effect-Plot
+#'
+#' Soll nur im Notfall geladen werden geht nicht Mehr
+#' 
+#' @param x Model fit
+#' @param selection,rows,cols,ask,graphics alles nicht ändern
+#' @param xlab  xlabs Ändern
+#' @param ...
+#'
+#' @return Plot
+#'
+#' @examples
+#' library(effects)
+#' library(gridExtra)
+#' A = rnorm(100)
+#' B = rnorm(100)
+#' C = factor(rep(c("This", "That"), 50))
+#'
+#' #-- Modifiziert wegen xlab
+#'
+#' plot.efflist <- stp25:::plot.efflist
+#'
+#' ef <- allEffects(lm(A ~ B + C))
+#' plot(ef, xlab = c("Foo", "Bar"))
+plot.efflist <- function(x, selection,
+                                 rows, cols,
+                                 ask=FALSE, # nicht vom mir benutzt
+                                 graphics=TRUE,
+                                 lattice,
+                                 xlab,
+                                 ...){
+  # Next line added 8/23/17 along with lattice, also lattice arg above
+  lattice <- if(missing(lattice)) list() else lattice
+  if (!missing(selection)){
+    if (is.character(selection)) selection <- gsub(" ", "", selection)
+    return(plot(x[[selection]], ...))
+  }
+  effects <- gsub(":", "*", names(x))
+  # if (ask){
+  #   repeat {
+  #     selection <- menu(effects, graphics=graphics, title="Select Term to Plot")
+  #     if (selection == 0) break
+  #     else print(plot(x[[selection]], ...))
+  #   }
+  # }
+ #  else {
+
+  #effects:::mfrow
+    neffects <- length(x)
+    mfrow <- effects:::mfrow(neffects)
+    if (missing(rows) || missing(cols)){
+      rows <- mfrow[1]
+      cols <- mfrow[2]
+    }
+    for (i in 1:rows) {
+      for (j in 1:cols){
+        if ((i-1)*cols + j > neffects) break
+        more <- !((i-1)*cols + j == neffects)
+        lattice[["array"]] <- list(row=i, col=j, nrow=rows, ncol=cols, more=more)
+
+        if (missing(xlab)) {
+          print(plot(x[[(i - 1) * cols + j]], lattice = lattice, ...))
+        }  else {
+          k <- (i - 1) * cols + j
+          x_lab <-
+            if (is.null(names(xlab)))
+              xlab[k]
+          else
+            xlab[names(x[k])]
+
+          print(plot(x[[k]], lattice = lattice, xlab = x_lab, ...))
+
+        }
+      }
+    }
+  #}
+}
+
+
+
