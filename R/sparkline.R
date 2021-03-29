5b22c11831ab517ba00d145f397a201c932c0b3f
stp4 <w.peter@statistik-peter.at>
2021-03-29 09:47:43 +0200

    Zweite überarbeitung

--- R/sparkline.R
+++ R/sparkline.R
@@ -9,6 +9,49 @@
 #' @param include.max number
 #' @export
 #'
+#' @examples 
+#' 
+#'  set.seed(1)
+#' 
+#' DF <- data.frame(
+#'   Laborwert = gl(7, 8,
+#'                  labels = c(
+#'                    "Albumin", "Amylase", "Lipase",
+#'                    "AST", "ALT","Bilirubin","C-Peptid")),
+#'   Treat = gl(2, 4, labels = c("Control", "Treat")),
+#'   Time = gl(4, 1, labels = c("t0", "t1", "t2", "t4")),
+#'   x = rnorm(7 * 8)
+#' )
+#' DF <- transform(DF,
+#'                 x = scale(x + as.numeric(Treat)*2 + as.numeric(Time) / 2))
+#' DF1 <- stp25aggregate::Summarise(DF, x~ Laborwert+ Time, fun=mean )
+#' 
+#' names(DF1)[4]<- "x"
+#' #         )
+#' DF1[21,4]<-1
+#' 
+#' sparkplot(
+#'   x ~ Time | Laborwert,
+#'   DF1,
+#'   between = 1.5,
+#'   
+#'   
+#'   include.axis=TRUE,
+#'   pch = 19,
+#'   col="green",
+#'   include.first = TRUE,
+#'   #  as.table=FALSE,
+#'   include.max = TRUE,
+#'   include.labels=TRUE
+#' )  
+#' require(grid)
+#' 
+#' grid.text("A", .02, .95, gp=gpar(fontsize=20))
+#' grid.text("B", .02, .45, gp=gpar(fontsize=20))
+#' grid.lines(x = unit(c(.1, .95), "npc"),
+#'            y = unit(c(.065, 0.065), "npc"),
+#'            arrow =arrow(angle = 30, length = unit(0.1, "inches"),
+#'                         ends = "last", type = "open"))
 panel.sparkline <- function(x,
                             y,
                             ...,
@@ -19,10 +62,20 @@ panel.sparkline <- function(x,
                             cex.arrows= NULL,
                             cex.points=.8,
                             lim.arrows=.25,
+                            pch=NULL,
+                            pch.default=16,
+                            col.default="gray40",
+                            col.max.min= c("blue", "red"), 
                             include.labels = TRUE,
                             include.arrows = TRUE, 
                             include.max=TRUE,
+                            include.min=TRUE,
+                            include.first=FALSE,
+                            include.last=FALSE,
                             include.box =TRUE) {
+  
+  #print(pch)
+
   
   if(!is.logical(include.box)){
     box.x1 <-min(x)
@@ -48,16 +101,13 @@ panel.sparkline <- function(x,
     col = "grey90",  border = "grey90")
   }
   
-  lattice::panel.xyplot(
-    x, y, ...)
-  
+  lattice::panel.xyplot(x, y, ...)
+  #lattice::panel.xyplot(x, y,type="p", ...)
+  if(!is.null(pch))
   lattice::panel.points(
-    x[which.max(y)], max(y),
-    pch = 16, cex = cex.points, col = "blue")
+    x, y, pch = pch, cex = cex.points, col=col.default)
   
-  lattice::panel.points(
-    x[which.min(y)], min(y),
-    pch = 16, cex = cex.points, col = "red")
+
   
   grid::pushViewport(
     grid::viewport(
@@ -65,17 +115,59 @@ panel.sparkline <- function(x,
       yscale = grid::current.viewport()$yscale,
       clip = "off"
     ))
+
+  if (include.first | include.last) {
+    last <- c(x[1], y[1])
+    first <-  c(x[length(x)], y[length(x)])
+ 
+    if (first[2] < last[2]) {
+      first <- c(x[1], y[1])
+      last  <-  c(x[length(x)], y[length(x)])
+    }
+    lattice::panel.points( x=c(first[1],last[1]),
+                  y=c(first[2],last[2]),
+                  pch = pch.default,
+                  cex = cex.points, col= col.default
+                  )
+
+    lattice::panel.text(
+      first[1], first[2],
+      labels = signif(first[2], digits),
+      cex = cex.numbers,
+      fontfamily = "serif",
+      adj = c(0.5, -.75)
+    )
+    
+    lattice::panel.text(
+      last[1], last[2],
+      labels = signif(last[2], digits),
+      cex = cex.numbers,
+      fontfamily = "serif",
+      adj = c(0.5, 1.5)
+    )
+  }
   
   
-  if (include.max){
+  
+  if (include.max|include.min) {
+    lattice::panel.points(
+      x = c(x[which.max(y)], x[which.min(y)]),
+      y = c(max(y), min(y)),
+      pch = pch.default,
+      cex = cex.points,
+      col = col.max.min
+    )
+   
+    if (! (include.first | include.last) ){
     lattice::panel.text(
       x = x[which.max(y)],
       y = max(y),
       labels = signif(max(y), digits),
       cex = cex.numbers,
       fontfamily = "serif",
-      adj = c(0.5,-.75)
+      adj = c(0.5, -.75)
     )
+  
     lattice::panel.text(
       x = x[which.min(y)],
       y = min(y),
@@ -84,20 +176,34 @@ panel.sparkline <- function(x,
       fontfamily = "serif",
       adj = c(0.5, 1.5)
     )
+    }
   }
   
-  if (include.labels) {lattice::panel.text(
-    x = head(x, n = 1),
+  if (include.labels) {
+    lattice::panel.text(
+    x = x[1],
     y = mean(y),
     labels = z.name[lattice::panel.number()],
     fontfamily = "serif",
     pos = 2,
-    cex=cex.label
-  )}
+    cex=cex.label,
+    offset = 1
+    )
+    }
   
   if (include.arrows) {
     draw_arrow(x, y, list(...), cex.arrows, lim.arrows)
   }
+  
+
+  # grid.lines(x = unit(c(0, 1), "npc"),
+  #            y = unit(c(-0.6, -0.6), "npc")#,
+  #            # default.units = "npc",
+  #            # arrow = NULL, name = NULL,
+  #            # gp=gpar(), draw = TRUE, vp = NULL,
+  #            # 
+  #            
+  #            )
   
   grid::popViewport()
   
@@ -118,7 +224,11 @@ panel.sparkbar <- function(x,
                            lim.arrows=.25,
                            include.labels = TRUE,
                            include.arrows = TRUE, 
-                           include.max=TRUE) {
+                           include.max=TRUE,
+                           include.min=TRUE,
+                           include.first=FALSE,
+                           include.last=FALSE
+                           ) {
   
   lattice::panel.barchart(x, y,  
                           # box.ratio = 1, #box.width = box.ratio/(1 + box.ratio), 
@@ -295,7 +405,7 @@ sparkplot <- function(x,
                       lwd = 2,
                       lty = NULL,
                       col = NULL,
-                      pch = 15,
+                      pch = NULL,
                       type = "l",
                       scales,
                       strip,
@@ -304,15 +414,19 @@ sparkplot <- function(x,
                       xlab = "",
                       digits = 3,
                       between = 1,
+                      
                       right.padding = 4,
                       left.padding =1.5,
                       include.labels = TRUE,
                       include.arrows = TRUE,
                       include.axis= FALSE,
                       include.max=TRUE,
+                      include.min=include.max,
+                      include.first=FALSE,
+                      include.last=include.first,
                       include.box=TRUE,
                       ...) {
-  if (type == "barchart")
+  if (type == "barchart"){
     return(
       sparkplot2(
         x,
@@ -333,9 +447,10 @@ sparkplot <- function(x,
         include.max =   include.max,
         ...
       )
-      
     )
+  }
   
+  # Daten vorgereiten
   # lhs <- x[[2L]]
   x.var <-   all.vars(x)[2]
   rhs <- all.vars(x)[3]
@@ -363,21 +478,32 @@ sparkplot <- function(x,
     scales <-  list(
       y = list(at = NULL, relation = "free"),
       x = list(
-        fontfamily = "serif",
-        at = at,
+        fontfamily = "serif",  
+        at = at,  
         label = label
+        #,  col="red"
+    
       )
     )
-  else   scales <-  list(
-    y = list(at = NULL, relation = "free"),
-    x= list(at=NULL)
+  else   scales <- list(
+    y = list(at = NULL,  relation = "free"), 
+    x = list(at=NULL)
   )
   
+  
+  #trellis.par.get("axis.line")
+  #  "axis.text"         ""
   par.settings <- list(
     axis.line = list(col = "transparent"),
+    axis.components =  list(bottom = list(
+      tck = 1, pad1 = 2, pad2 = 0
+    )),
     layout.widths = list(
       right.padding = right.padding,
-      left.padding =   if( include.labels)   left.padding + max(nchar(z.name) / 2) else left.padding
+      left.padding =   if (include.labels)
+        left.padding + max(nchar(z.name) / 2)
+      else
+        left.padding
     ),
     superpose.symbol = list(pch = pch, fill = col, col = col) ,
     superpose.polygon = list(col = col, border = "transparent"),
@@ -386,37 +512,41 @@ sparkplot <- function(x,
   )
   
   
-  lattice::xyplot(
-    x,
-    data,
-    xlab = xlab,
-    ylab = ylab,
+ lattice::xyplot(
+    x, data,
+    xlab = xlab, ylab = ylab,
     strip = FALSE,
     lwd = lwd,
     type = type,
     layout = c(1, length(unique(data[[rhs]]))),
     between =  list(y = between),
     scales = scales,
-    
+   # as.table=TRUE,
     par.settings = par.settings,
     panel = function(x, y, ...) {
-      panel.sparkline (
-        x,
-        y,
+      panel.sparkline(
+        x,  y,
         ...,
         #  data = data,
+        pch=pch,
         z.name = z.name,
         digits = digits,
         include.labels = include.labels,
         include.arrows = include.arrows,
         include.max=include.max,
+        include.min=include.min,
+        include.first=include.first,
+        include.last=include.last,
         include.box=include.box
       )
+     #lims <- current.panel.limits()
+      #print(lims)
+      
+ 
     },
     ...
   )
-  
-  
+ 
 }
 
 #' @rdname sparkplot
@@ -426,7 +556,7 @@ sparkplot2 <- function(x,
                        lwd = 2,
                        lty = NULL,
                        col = NULL,
-                       pch = 15,
+                       pch = NULL,
                        type = "barchart",
                        scales,
                        strip,
@@ -532,342 +662,7 @@ sparkplot2 <- function(x,
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # http://www.motioninsocial.com/tufte/#sparklines
-# #require(tidyverse)
-# #require(stp25aggregate)
-# 
-# # panel.sparkline
-# #
-# # @param x,y,... from panel
-# 
-# # @param z.name names of levels
-# # @param digits to signif
-# #
-# # @return
-# # @export
-# #
-# 
-# panel.sparkline <- function(x, y, 
-#                             ..., 
-#                            # data, 
-#                             z.name, 
-#                             digits = 2) {
-#   
-#   lattice::panel.rect(
-#     min(x), quantile(y, 0.25),
-#     max(x), quantile(y, 0.75),
-#     col = "grey90", border = "grey90")
-#   
-#   lattice::panel.xyplot(x, y, ...)
-#   
-#   grid::pushViewport(
-#     grid::viewport(
-#       xscale = grid::current.viewport()$xscale,
-#       yscale = grid::current.viewport()$yscale,
-#       clip = "off"))
-#   
-# 
-#   lattice::panel.text(  # label
-#     x = head(x, n = 1),
-#     y = head(y, n = 1),
-#     labels = z.name[lattice::panel.number()],
-#     fontfamily = "serif",  pos = 2)
-#   
-#   lattice::panel.text(
-#     x = x[which.max(y)],
-#     y = max(y),
-#     labels = signif(max(y), digits),
-#     cex = 0.8, fontfamily = "serif", adj = c(0.5, -.75))
-#   
-#   lattice::panel.text(
-#     x = x[which.min(y)],
-#     y = min(y),
-#     labels = signif(min(y), digits),
-#     cex = 0.8, fontfamily = "serif", adj = c(0.5, 1.5))
-#   
-#   grid::popViewport()
-#   
-#   lattice::panel.points(
-#     x[which.max(y)],
-#     max(y),
-#     pch = 16, cex = 1, col = "blue")
-#   
-#   lattice::panel.points(
-#     x[which.min(y)],
-#     min(y),
-#     pch = 16, cex = 1, col = "red")
-# }
-# 
-# 
-# 
-# 
-# # sparkplot
-# #
-# # @param x,data to xyplot() 
-# # @param lwd,lty,col lines
-# # @param pch used in auto.key
-# # @param type "l 
-# # @param scales,strip,layout not used
-# # @param ylab,xlab labels
-# # @param digits to signif
-# # @param  right.padding,left.padding links rechts abstand =1,
-# # @param ... 
-# #
-# # @return lattice
-# # @export
-# #
-# # @examples
-# # 
-# # 
-# # 
-# # 
-# # 
-# # DF12 <- data.frame(
-# #   souce = factor(rep(
-# #     c(
-# #       "CD45+ Leukocytes",  "CD3+ T cells",
-# #       "CD19+ B cells",   "CD56+ NK cells",
-# #       "Monocytes" ,     "Granulocytes"
-# #     ),  each = 5)),
-# #   time =  factor(rep(c(1, 3, 6, 12, 24), 6)),
-# #   value = c(
-# #     541,   482,  510,  402,  293,
-# #     203,   151,  144,  118,  192,
-# #     252,   352,  460,  553,  635,
-# #     99,     88,   96,   75,   51,
-# #     37,     52,   64,   39,   30,
-# #     1988, 1947, 1219, 1972, 1643
-# #   )
-# # )
-# # 
-# # p1 <- sparkplot(value ~ time | souce,
-# #                 DF12)
-# # 
-# # set.seed(1)
-# # 
-# # DF <- data.frame(
-# #   Laborwert = gl(
-# #     7,
-# #     8,
-# #     labels = c(
-# #       "Albumin",  "Amylase",    "Lipase",
-# #       "AST",  "ALT",  "Bilirubin",  "C-Peptid"
-# #     )
-# #   ),
-# #   Treat = gl(2, 4, labels = c("Control", "Treat")),
-# #   Time = gl(4, 1, labels = c("t0", "t1", "t2", "t4")),
-# #   x = rnorm(7 * 8)
-# # )
-# # DF <- transform(DF,
-# #                 x = x + as.numeric(Treat) + as.numeric(Time) / 2)
-# # 
-# # #stp25output::reset_lattice()
-# # 
-# # p2 <- sparkplot(
-# #   x ~ Time | Laborwert,
-# #   DF,
-# #   groups = Treat,
-# #   lwd = 1,
-# #   #lty=1:2,
-# #   col =  c("purple", "darkgreen"),
-# #   # auto.key=list(),
-# #   key = list(
-# #     corner = c(1, 1.1),
-# #     lines = list(col = c("purple", "darkgreen"), lwd = 2),
-# #     # title="CIT",
-# #     cex = .75,
-# #     text = list(levels(DF$Treat))
-# #   )
-# # )
-# # 
-# # p2
-# # 
-# # DF$Time <- as.numeric(DF$Time) + rnorm(nrow(DF), .1, .1)
-# # 
-# # 
-# # p3 <- sparkplot(
-# #   x ~ Time | Laborwert,
-# #   DF,
-# #   groups = Treat,
-# #   lwd = 1,
-# #   col =  c("purple", "darkgreen"),
-# #   digits=1,
-# #   key = list(
-# #     corner = c(1, 1.1),
-# #     lines = list(col = c("purple", "darkgreen"), lwd = 2),
-# #     cex = .75,
-# #     text = list(levels(DF$Treat)),
-# #     columns = 2
-# #   )
-# # )
-# # 
-# # p3
-# # 
-# # #library(cowplot)
-# # # plot_grid(p3,
-# # #           p2,
-# # #           rel_widths = c(1, 1)
-# # #           ,
-# # #           labels = c('Main', 'CIT'))
-# # 
-# 
-# sparkplot <- function(x,
-#                       data,
-#                       lwd = 2,
-#                       lty = NULL, col = NULL, pch = 15,
-#                       type = "l",
-#                       scales, strip, layout,
-#                       ylab = "", xlab = "",
-#                       digits = 3,
-#                       right.padding = 1,
-#                       left.padding =1.5,
-#                       ...) {
-#  # lhs <- x[[2L]]
-#   x.var <-   all.vars(x)[2]
-#   rhs <- all.vars(x)[3]
-#   data <- data[order(data[[x.var]]),]
-# 
-#   if (is.null(col)) col <- 1
-#   if (is.null(lty))  lty <- 1
-#   
-#   
-#   if (!is.factor(data[[rhs]]))
-#     data[[rhs]] <- factor(data[[rhs]])
-#   
-#   z.name <- levels(data[[rhs]]) 
-#   
-#   if (is.factor(data[[x.var]])) {
-#     at <- 1:nlevels(data[[x.var]])
-#     label <- levels(data[[x.var]])
-#     data[[x.var]] <- as.numeric(data[[x.var]])
-#   }
-#   else{
-#     at <- label <-  pretty(data[[x.var]])
-#   }
-#   
-#    
-#   scales <-  list(
-#     y = list(at = NULL, relation = "free"),
-#     x = list(
-#       fontfamily = "serif",
-#       at = at, label = label))
-# 
-#   par.settings <- list(
-#     axis.line = list(col = "transparent"),
-#     layout.widths = list(
-#           right.padding = right.padding,
-#           left.padding = left.padding + max(nchar(z.name) / 2)),
-#     superpose.symbol = list(pch = pch, fill = col, col = col) ,
-#     superpose.polygon = list(col = col, border = "transparent"),
-#     superpose.line = list(col = col, lty = lty),
-#     strip.shingle = list(col = col)
-#   )
-#   
-#   
-#   lattice::xyplot(
-#     x,
-#     data,
-#     xlab = xlab,
-#     ylab = ylab,
-#     strip = FALSE,
-#     lwd = lwd,
-#     type = type,
-#     layout = c(1, length(unique(data[[rhs]]))),
-#     between = list(y = 1),
-#     scales = scales,
-#     
-#     par.settings = par.settings,
-#     panel = function(x, y, ...) {
-#       panel.sparkline (x,
-#                        y,
-#                        ...,
-#                      #  data = data,
-#                        z.name = z.name,
-#                        digits = digits)
-#     },
-#     ...
-#   )
-#   
-#   
-# }
-# 
 
 
-
-
-# 
-# set.seed(1)
-# 
-# DF <- data.frame(
-#   Laborwert = gl(7, 8,
-#                  labels = c(
-#                    "Albumin", "Amylase", "Lipase",
-#                    "AST", "ALT","Bilirubin","C-Peptid")),
-#   Treat = gl(2, 4, labels = c("Control", "Treat")),
-#   Time = gl(4, 1, labels = c("t0", "t1", "t2", "t4")),
-#   x = rnorm(7 * 8)
-# )
-# DF <- transform(DF,
-#                 x = scale(x + as.numeric(Treat)*2 + as.numeric(Time) / 2))
-# DF1 <- stp25aggregate::Summarise(DF, x~ Laborwert+ Time, fun=mean )
-# names(DF1)[4]<- "x"
-# 
-# #: "p", "l", "h", "b", "o", "s", "S", "r", "a", "g"
-# p1 <- sparkplot(x ~ Time | Laborwert,
-#                 DF1,
-#                 between = 1.5,
-#                 include.box =
-#                   data.frame(
-#                     up = c(-.3, -.3, -.3, -.3, -.3, -.3, -.03),
-#                     low = c(-.92,-.92, -.92, -.92, -.92, -.92, 1.2)
-#                   )
-#                 )
-# 
-# col<- c("purple", "darkgreen")
-# p2<- sparkplot(
-#   x ~ Time | Laborwert,
-#   DF,
-#   groups = Treat,
-#   between=1.5,
-#   include.labels = FALSE, 
-#   left.padding=-5,  right.padding=3,
-#   col = col ,
-#   key = list(
-#     corner = c(1, 1.1),
-#     lines = list(col = col, lwd = 2),
-#     # title="CIT",
-#     cex = .75,
-#     columns = 2,
-#     text = list(levels(DF$Treat))
-#   )
-# )
-# 
-# 
-# #windows(8,4)
-# require(cowplot)
-# plot_grid(p1,  p2,
-#           nrow=1,
-#           rel_widths = c(1, .5)
-# )
-# 
 
