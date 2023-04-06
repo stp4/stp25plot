6b9f747ede1bf57986714122fc5ee33427f039e4
stp4 <w.peter@statistik-peter.at>
2023-04-06 10:38:31 +0200

    Dokumentation

--- R/4-likertplot.R
+++ R/4-likertplot.R
@@ -11,8 +11,9 @@
 #' @param rightAxis,as.percent,ReferenceZero,reference.line.col,col.strip.background
 #' an HH:::plot.likert.formula
 #' @param positive.order das nicht verwenden - wird ueber Tbll_likert gesteuert
-#' @param auto.key,columns,space,between   columns = 2,
-#' @param ... HH:::plot.likert.formula
+#' @param auto.key,columns,space  columns = 2,
+#' @param ... HH:::plot.likert.formula  
+#'    between=list(x=0))
 #'
 #' @return lattice Plot
 #' @export
@@ -43,24 +44,35 @@
 #' 
 #' #require(HH)  # ?likertplot
 #' class(Res2)
-#' windows(7, 3)
+#' #windows(7, 3)
 #' #attr(Res2, "plot")$results 
-#' likertplot(Item   ~ . | Geschlecht , data = Res2)
+#' likertplot(Item ~ . | Geschlecht,
+#'            data = Res2,    
+#'             between=list(x=0))
 #' 
+#'  # col = likert_col(attr(data, "plot")$nlevels, middle = ReferenceZero)
+#'   
+#' DF2 %>% likert_plot(Magazines, Comic.books, Fiction, Newspapers,
+#'                     relevel = letters[1:5],
+#'                     ReferenceZero = 1.5,
+#'                     columns=5)
 #' 
+#' #DF2 %>% Likert(Magazines, Comic.books, Fiction, Newspapers) %>% likertplot()
+
 likertplot <- function(x = Item   ~ . ,
-                       data,
+                       data=NULL,
                        main = '',
                        ylab = "",
                        sub = "",
-                       xlab = "Prozent",
+                       xlab = if(horizontal) {if(as.percent) "Prozent" else "Anzahl"} else "",
                        col = NULL,
                        rightAxis = FALSE,
                        positive.order = FALSE, 
                        as.percent = TRUE,
                        auto.key = list(space = space, 
                                        columns = columns,
-                                       between = between),
+                                       between = 1
+                                       ),
                        
                        ReferenceZero = NULL,
                        reference.line.col = "gray65",
@@ -68,49 +80,68 @@ likertplot <- function(x = Item   ~ . ,
                        wrap = TRUE,
                        columns = 2,
                        space ="top",
-                       between = 1,
+                       horizontal = TRUE,
+                       between = list(x = 1 + (horizontal), 
+                                      y = 0.5 +2 * (!horizontal)),
+                     #  between.key = 1,
                        ...) {
+  name_item <- "Item"
   
-  if (is.data.frame(x)) {
-    data <- x
-    x <- attr(x, "plot")$formula
+  if(is.null(data)){
+    if(is.data.frame(x) & ("plot" %in% names(attributes(x))) ){
+      # Tbll_likert() 
+      formula <-  attr(x, "plot")$formula
+      nlevels <-  attr(x, "plot")$nlevels
+      data <-  attr(x, "plot")$results
+    }
+    else if(inherits(x, "likert")){
+      formula <- x$formula
+      nlevels <- x$nlevels
+      data <-  x$results
+    }
+    else{ stop("No data.frame !") }
+  }
+  else if (plyr::is.formula(x)) {
+   if (is.data.frame(data) ) {
+     if("plot" %in% names(attributes(data))){
+       # Tbll_likert()
+       formula <-  x
+       nlevels <-  attr(data, "plot")$nlevels
+       data <-  attr(data, "plot")$results
+     }
+     else{
+       formula <-  x
+       name_item<- all.vars(x)[1]
+     }
+   }
+    else if( inherits(x, "likert") ){
+       formula <- x
+       nlevels <- data$nlevels
+       data <-  data$results
+      }
   }
   
   
-  if (!is.null(attr(data, "plot"))) {
-    
-    if (is.null(ReferenceZero)) {
-      ReferenceZero <-  attr(data, "plot")$ReferenceZero
-    }
-    
-    
-    if (is.null(col)) {
-      col <- if (is.null(ReferenceZero))
-        likert_col(attr(data, "plot")$nlevels)
-      else
-        likert_col(attr(data, "plot")$nlevels,
-                   middle = ReferenceZero)
-    }
-    
-    
-    if (is.logical(wrap)) {
-      if (wrap) {
-        attr(data, "plot")$results$Item <-
-          stp25tools:::wrap_sentence(as.character(attr(data, "plot")$results$Item),
-                        35)
-        
-      }
-    } 
-    else{
-      attr(data, "plot")$results$Item <-
-        stp25tools:::wrap_sentence(as.character(attr(data, "plot")$results$Item),
-                      wrap)
-      
-    }
-    
-    HH:::plot.likert.formula(
-      x,
-      attr(data, "plot")$results,
+  
+
+  if (is.null(col)) {
+    col <- if (is.null(ReferenceZero)) likert_col(nlevels)
+           else likert_col(nlevels, middle = ReferenceZero)
+  }
+  
+  if (is.logical(wrap)) {
+    if (wrap) 
+      data[[name_item]] <- stp25tools:::wrap_sentence( data[[name_item]], 35)
+  } 
+  else{
+    data[[name_item]] <- stp25tools:::wrap_sentence( data[[name_item]], wrap)
+  }
+  
+  
+  
+  HH:::plot.likert.formula(
+      x = formula,
+      data = data,
       main = main,
       ylab = ylab,
       sub = sub,
@@ -123,72 +154,97 @@ likertplot <- function(x = Item   ~ . ,
       ReferenceZero =  ReferenceZero,
       reference.line.col = reference.line.col,
       col.strip.background = col.strip.background,
-      
+      between=between,
+      horizontal = horizontal,
       ...
+  
     )
-  }
-  else if (inherits(data, "likert")) {
-    
-    
-    if(is.null(ReferenceZero)){ 
-      ReferenceZero<-  data$ReferenceZero 
-    } 
-    
-    
-    if (is.null(col)){
-      col <- if (is.null(ReferenceZero))
-        likert_col(data$nlevels)
-      else
-        likert_col(data$nlevels,
-                         middle = ReferenceZero)}
-    
-    
-    HH:::plot.likert.formula(
-      x,
-      data$results,
-      
-      main = main,
-      ylab = ylab,
-      sub = sub,
-      xlab = xlab,
-      col = col,
-      rightAxis = rightAxis,
-      positive.order = positive.order,
-      as.percent = as.percent,
-      auto.key = auto.key,
-      ReferenceZero = ReferenceZero,
-      reference.line.col = reference.line.col,
-      col.strip.background = col.strip.background,
-      
-      ...
-    )
-  }
-  else{
-    if (is.null(col))
-      col <-  stp25plot::farbe("likert.blue.red", 5)
-    HH:::plot.likert.formula(
-      x,
-      data,
-      main = main,
-      ylab = ylab,
-      sub = sub,
-      xlab = xlab,
-      col = col,
-      rightAxis = rightAxis,
-      positive.order = positive.order,
-      as.percent = as.percent,
-      auto.key = auto.key,
-      ReferenceZero = ReferenceZero,
-      reference.line.col = reference.line.col,
-      col.strip.background = col.strip.background,
-      ...
-    )
-  }
+ 
   
   
 }
 
 
+#' @rdname likertplot
+#'
+#' @param ... an Tbll_likert
+#'
+#' @return HH likertplot
+#' @export
+#'
+#' @examples
+#' 
+#' DF2 %>% likert_plot(Magazines, Comic.books, Fiction, Newspapers)
+#' 
+likert_plot <- function(...,
+                        main = '',
+                        ylab = "",
+                        sub = "",
+                        xlab = if(as.percent) "Prozent" else "Anzahl",
+                        col = NULL,
+                        rightAxis = FALSE,
+                        positive.order = FALSE, 
+                        as.percent = TRUE,
+                        auto.key = list(space = space, 
+                                        columns = columns,
+                                        between = 1
+                        ),
+                        ReferenceZero = NULL,
+                        reference.line.col = "gray65",
+                        col.strip.background = "gray97",
+                        wrap = TRUE,
+                        columns = 2,
+                        space ="top",
+                        relevel = NULL,
+                        horizontal = TRUE,
+                        between = list(x = 1 + (horizontal), 
+                                       y = 0.5 +2 * (!horizontal)),
+                        par.strip.text = list(lines = 1, cex = .8)
+                        ){
+  if (is.null(relevel)){
+    X <- stp25stat2:::Likert(...)
+   }
+  else{
+    X_old <-  stp25tools::prepare_data2(...)
+    
+    X_old$data[X_old$measure.vars] <-
+      stp25tools::dapply2(
+        X_old$data[X_old$measure.vars],
+        fun = function(x) {
+          if (nlevels(x) == length(relevel))
+            levels(x) <- relevel
+          else
+            stop("\nDie relevel stimmen in der laenge nicht überein!\n")
+          x
+        }
+      )
+    X <- stp25stat2:::Likert(X_old$formula,  X_old$data)
+  }
+  
+
+  
+
+ likertplot(
+ X,
+    main = main,
+    ylab = ylab,
+    sub = sub,
+    xlab = xlab,
+    col = col,
+    rightAxis = rightAxis,
+    positive.order = positive.order,
+    as.percent = as.percent,
+    auto.key = auto.key,
+    ReferenceZero = ReferenceZero,
+    reference.line.col = reference.line.col,
+    col.strip.background = col.strip.background,
+    wrap = wrap,
+    horizontal = horizontal,
+    between = between,
+    par.strip.text = par.strip.text
+
+  )
+  }
 
 #' likert_col
 #'
