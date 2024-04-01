4a5ad60c70826688827171f6912c794c8bdbb411
stp4 <w.peter@statistik-peter.at>
2024-04-01 12:58:27 +0200

    New funktion plot_allEffects Workaround for effect.plot

--- R/plot.efflist.R
+++ R/plot.efflist.R
@@ -1,8 +1,50 @@
-#' Workaraund für plot
+#' Workaround foreffects:::plot.eff
 #'
+#' @name plot.efflist
 #' @param ... alles an plot
 #'
 #' @return standard plot
+#' @export
+plot_allEffects <- function(x, 
+                            main = NULL,
+                            factor.names = FALSE,
+                            rel_widths = 1, rel_heights = 1,
+                            ylab = NULL, xlab = NULL, labels = NULL,
+                            cex = 1.1,
+                            axis.padding = .4,
+                            multiline =NULL,
+                            x.var = 1,
+                            space = "right",
+                            columns = 1,
+                            ...) {
+  if (inherits(x, "efflist"))
+    plot2(x, main = main,
+          factor.names = factor.names,
+          rel_widths = rel_widths, rel_heights = rel_widths,
+          ylab = ylab, xlab = xlab, labels = labels,
+          cex = cex,
+          axis.padding = axis.padding,
+          multiline = multiline,
+          x.var = x.var,
+          space = space,
+          columns = columns,...)
+  else
+    plot2(effects::allEffects(x), 
+          main = main,
+          factor.names = factor.names,
+          rel_widths = rel_widths, rel_heights = rel_widths,
+          ylab = ylab, xlab = xlab, labels = labels,
+          cex = cex,
+          axis.padding = axis.padding,
+          multiline = multiline,
+          x.var = x.var,
+          space = space,
+          columns = columns,
+          ...)
+}
+
+
+#' @rdname plot.efflist
 #' @export
 plot2 <- function(...){
   UseMethod("plot2")
@@ -10,128 +52,95 @@ plot2 <- function(...){
 
 
 #' @export
-#' @rdname plot2
+#' @rdname plot.efflist
 plot2.default <- function(...){ plot(...) }
 
 
-#' @rdname plot2
+#' @rdname plot.efflist
 #' 
 #' @description
 #'  method for class 'eff'  effects::allEffects
 #' 
 #' @param x efflist-object
 #' @param main 	the title for the plot, 
-#' @param factor.names  lattice
-#' @param nrow,ncol   cowplot::plot_grid
-#' @param lattice an optional list
+#' @param factor.names  lattice factor.names = FALSE
+#' @param multiline,x.var  multiline display a multiline plot in each panel
+#'  x.var = 1,  bei interactionen immer was in der Formel an erster stelle steht description
+#' @param nrow,ncol  an cowplot::plot_grid
+#' @param rel_widths,rel_heights an cowplot::plot_grid
 #' @param bottom.padding,top.padding,left.padding,right.padding,axis.padding lattice::lattice.options
 #' @param lty line type for the loess smooth; the default is the first line type, normally 1 (a solid line).
 #' @param lty.factor line type for the loess smooth; 0.
 #' @param plot plot or list cowplot::plot_grid(plotlist)
 #' @param rug  rug
 #' @param ... an effects::plot.eff
-#'
+#' @param ylab,xlab,labels x und y Beschriftung labels ist ein Vector mit Namen
+#' @param cex,cex.x,cex.y größe der Schrift
+#' @param axes liste wird automatisch aud den labels erstellt specifications for the x and y axes
+#' @param key.args,space,columns a key, or legend, is added to the plot if multiline = TRUE
+#' @param par.settings siehe https://stackoverflow.com/questions/13026196/how-to-nicely-rescale-lattice-figures
 #' @return list oder plot
 #' @export
 #'
 #' @examples
 #' 
-#'  n <- 100
-#' set.seed(2)
-#' DF <- data.frame(
-#'   y = rnorm(n),
-#'   a = rnorm(n),
-#'   b = rnorm(n),
-#'   c = rnorm(n),
-#'   d = rnorm(n),
-#'   e = rnorm(n),
-#'   f = rnorm(n),
-#'   g = gl(2, n / 2, labels = c("Control", "Treat"))
-#' ) |>
-#'   transform(
-#'     y = y + a + b + d + as.numeric(g),
-#'     e = cut(e, 2, c("low", "hig")),
-#'     b = factor(as.character(cut(b, 3, c(
-#'       "a", "c", "b"
-#'     ))))
-#'   )
+#' # require(stp25plot)
+#' # require(stp25tools)
+#' # require(stp25stat2)
 #' 
 #' 
-#' fit <- lm(y ~ a + b + c + g * d + e + g * b, DF)
-#' #fit2 <- step(fit)
-#' e1 <- effects::allEffects(fit)
-#' 
-#' p1 <-
-#'   plot2(e1,
-#'         symbols = list(pch = 15, cex = 1.2),
-#'         axes = list(x = list(cex = .9)))
-#' p1
-#' #cowplot::plot_grid(plotlist =p1)
-#' 
-#' #' require(effects)
-#' mtcars2 <- within(mtcars, {
-#'   vs <- factor(vs, labels = c("V", "S"))
-#'   am <- factor(am, labels = c("automatic", "manual"))
-#'   cyl  <-  (cyl)
-#'   cyl_ord  <-  ordered(cyl)
-#'   gear <- ordered(gear)
-#'   carb <- ordered(carb)
-#' })
-#' 
-#' mtcars2$mpg[mtcars2$cyl_ord==6]  <- mtcars2$mpg[mtcars2$cyl_ord==6] *.5
-#' 
-#' mtcars2 <- mtcars2 |> stp25tools::Label(
-#'   mpg	= "Miles/(US) gallon",
-#'   cyl_ord	= "Number of cylinders",
-#'   disp	= "Displacement (cu.in.)",
-#'   hp	= "Gross horsepower",
-#'   drat =	"Rear axle ratio",
-#'   wt =	"Weight (1000 lbs)",
-#'   qsec =	"1/4 mile time",
-#'   vs =	"Engine (0 = V-shaped, 1 = straight)",
-#'   am	= "Transmission",
-#'   gear	= "Number of forward gears",
-#'   carb =	"Number of carburetors"
+#' mtcars2 <- mtcars |> 
+#' dplyr::mutate(
+#'   vs   = factor(vs, labels = c("V-shaped", "straight")),
+#'   am   = factor(am, labels = c("automatic", "manual")),
+#'   cyl  = ordered(cyl),
+#'   gear = ordered(gear),
+#'   carb = ordered(carb)
+#' ) |> Label(
+#'   mpg	 = "Miles/(US) gallon",
+#'   cyl	 = "Number of cylinders",
+#'   disp = "Displacement (cu.in.)",
+#'   hp	 = "Gross horsepower",
+#'   drat = "Rear axle ratio",
+#'   wt   = "Weight (1000 lbs)",
+#'   qsec = "1/4 mile time",
+#'   vs   = "Engine",
+#'   am   = "Transmission",
+#'   gear = "Number of forward gears",
+#'   carb = "Number of carburetors"
 #' )
 #' 
-#' #fit <- lm(mpg ~ hp * wt + am +cyl  , data = mtcars2)
-#' fit2 <- lm(mpg ~ hp * wt + am + cyl_ord  , data = mtcars2)
+#' fit <- lm(mpg ~ hp * wt + vs +  am * cyl  , data = mtcars2)
 #' 
+#' # lattice::trellis.par.set(effectsTheme())
+#' lattice::trellis.par.set(bw_theme(farbe(n=5), lwd = 1))
 #' 
-#' 
-#' ### Meine Version vs plot.efflist
-#' 
-#' 
-#' #plot( effects::allEffects(fit2) )
-#' e2<- effects::allEffects(fit2)
-#' names(e2)
-#' 
-#' p2<-plot2(e2,
-#'           labels = stp25tools::get_label(mtcars2),
-#'           plot=FALSE)
-#' 
-#' cowplot::plot_grid(plotlist =p2, 
-#'                    labels = c('A', 'B', 'C'),
-#'                    # scale = c(1, .9, .8),
-#'                    rel_heights = c(3,4))
+#' plot_allEffects(
+#'   fit,
+#'   labels = get_label(mtcars2),
+#'   main = letters[1:3],  #as.roman(1:3),
+#'   # cex= 1,
+#'   space = "right",
+#'   columns = 1,
+#'   rel_widths = c(3, 4),
+#'   rel_heights = c(5, 6)
+#' ) 
 plot2.efflist <-
   function (x,
-            main = "",
+            main = NULL,
             factor.names = FALSE,
-            nrow = NULL,
-            ncol = NULL,
-            lattice = list(),
-            labels = NULL,
-            cex =.8,
-            cex.x = cex, cex.y = cex,
+            nrow = NULL, ncol = NULL,
+            rel_widths = 1, rel_heights = 1,
+            ylab = NULL, xlab = NULL, labels = NULL,
+            cex = 1.1,
+            cex.x = cex * .8, cex.y = cex * .8,
+            cex.xlab = cex, cex.ylab = cex,
+            cex.strip = cex * 1.2,
+            cex.title = cex, cex.key = cex * 0.75, 
             lty = 1,
             lty.factor = 0,
             axes = NULL,
-            # axis.padding = lattice::lattice.getOption("axis.padding"),
-            # layout.heights = lattice::lattice.getOption("layout.heights"),
-            # layout.widths = lattice::lattice.getOption("layout.widths"),
-            # bottom.padding = lattice::lattice.getOption("bottom.padding"),
-            # top.padding = lattice::lattice.getOption("top.padding"),
+           
             bottom.padding = 0,
             top.padding = 0,
             left.padding = .4,
@@ -142,41 +151,72 @@ plot2.efflist <-
             plot = TRUE,
             rug = FALSE,
             multiline =NULL,
+            x.var = 1,
+            space = "right",
+            columns = 1,
+            key.args = list(
+              space = space,
+              columns = columns,
+              cex.title = cex.title,
+              cex = cex.key
+            ),
+           par.strip.text = list(),
+            #https://stackoverflow.com/questions/13026196/how-to-nicely-rescale-lattice-figures
+           par.settings = list(
+             add.text       = list(cex = cex.strip),
+              par.zlab.text = list(cex = cex.xlab),
+              par.ylab.text = list(cex = cex.ylab),
+              par.xlab.text = list(cex = cex.xlab)), 
             ...)
   {
     plotlist <- list()
+    param <- purrr::map(x, \(xfit) names(xfit$variables))
+    x <- x[!duplicated(param)]
+    
     effects <- gsub(":", "*", names(x))
     eff_names <-
       c(x[[1]]$response, unlist(strsplit(effects, "\\*")))
     
-    if (!is.null(labels))
-      labels <-  labels[eff_names]
+    if (!is.null(labels)){
+      labels <- labels[eff_names]
+      unlabl <- which(is.na(labels))
+      labels[unlabl] <- eff_names[unlabl]
+      names(labels)[unlabl] <- eff_names[unlabl]
+    }
     else{
+      # ylab und xlab aufdröseln
       labels <- eff_names
       names(labels) <- eff_names
+      if (!is.null(ylab)) {
+        labels[1] <- ylab
+      }
+      if (!is.null(xlab)) {
+        for (i in seq_along(xlab))
+          if (!is.na(xlab[i]))
+            labels[i + 1] <- xlab[i]
+      }
     }
-    
+ 
     if(is.null(axes))
       axes <-  axis_list(effects = effects,
-                         response = x[[1]]$response,
                          labels, cex.x, cex.y)
     
-    lattice::lattice.options(
-      layout.heights = list(
-        bottom.padding = list(x = bottom.padding),
-        top.padding = list(x = top.padding)
-      ),
-      layout.widths =
-        list(
-          left.padding = list(x = left.padding),
-          right.padding = list(x = right.padding)
-        ),
-      axis.padding = list(numeric = axis.padding)
-    )
     
+    # für Reset Lattice 
+    layout.heights = lattice::lattice.getOption("layout.heights")
+    layout.widths = lattice::lattice.getOption("layout.widths") 
+    resetaxis.padding  <-  lattice::lattice.getOption("axis.padding")$numeric
+
+    
+    set_temp_lattice(bottom.padding,
+                     top.padding,
+                     left.padding,
+                     right.padding,
+                     axis.padding)
+
+
     
     for (i in seq_along(effects)) {
-      
       is_fctr <-
         unlist(purrr::map(x[[i]]$variables, \(x) x$is.factor))
       
@@ -184,50 +224,75 @@ plot2.efflist <-
       else lty2 <- lty
       
       if (is.null(multiline)) {
-        if (any(is_fctr)) multiline2 <- FALSE
-        else multiline2 <- TRUE
+        if (any(is_fctr)) multiline_i <- FALSE
+        else multiline_i <- TRUE
       }
-      else multiline2 <- multiline
-      #effects:::plot.eff
-      plotlist[[effects[i]]] <-
-        plot(
-          x[[i]],
-          factor.names = factor.names,
-          lattice = lattice,
-          main = main,
-          rug = rug,
-          lty = lty2,
-          axes = axes,
-          multiline = multiline2,
-          ...
+      else multiline_i <- multiline
+      
+      # das geht sicher einfacher 
+      effects_i <- unlist(strsplit(effects[i], "\\*"))
+      axes_i <- list( y = axes$y, x = list(cex= axes$x$cex))
+       for (j in effects_i)
+         axes_i$x[[j]]$lab <- axes$x[[j]]$lab
+
+      if (length(effects_i) == 2 & multiline_i) {
+      key.args$title <- axes$x[[effects_i[-x.var]]]$lab
+      }
+      
+      plotlist[[effects[i]]]  <-
+        update(
+          effects:::plot.eff(
+            x[[i]],
+            factor.names = factor.names,
+            main = "",
+            rug = rug,
+            lty = lty2,
+            axes = axes_i,
+            multiline = multiline_i,
+            key.args = key.args,
+            x.var = x.var,
+            ...
+          ),
+          par.settings = par.settings,
+          par.strip.text = par.strip.text
         )
+     
     }
+    
+    # Reset Lattice
+    set_temp_lattice(  layout.heights$bottom.padding$x,
+                       layout.heights$top.padding$x,
+                       layout.widths$left.padding$x,
+                       layout.widths$right.padding$x,
+                       resetaxis.padding)
+    
     
     if (plot)
       cowplot::plot_grid(plotlist = plotlist,
+
                          nrow = nrow,
-                         ncol = ncol)
+                         ncol = ncol,
+                         rel_widths = rel_widths,
+                         rel_heights  = rel_heights,
+                         labels = main
+                         
+                         )
     else
       plotlist
   }
 
+
 #' make a list for the axix
-axis_list <- function(
-                      effects,
-                      response,
+axis_list <- function(effects,
+                      # response,
                       labels = NULL,
                       cex.x = 1,
-                      cex.y = 1
-                      ) {
+                      cex.y = 1) {
   axs_list <- list(y = list(cex = cex.y))
-
-  eff_names <-
-    c(response, unlist(strsplit(effects, "\\*")))
+  axs_list$y$lab <- as.vector(labels[1])
+  axs_list$x <- list(cex = cex.x)
   
-  axs_list$y$lab <- as.vector(labels[response])
-  axs_list$x  = list(cex = cex.x)
-  
-  for (i in names(labels)) {
+  for (i in names(labels[-1])) {
     axs_list$x[[i]] <- list(lab = as.vector(labels[i]))
     
   }
@@ -236,10 +301,35 @@ axis_list <- function(
   
 }
 
+set_temp_lattice <- function( 
+    bottom.padding,
+                             top.padding,
+                             left.padding,
+                             right.padding,
+                             axis.padding) {
+  lattice::lattice.options(
+    layout.heights =
+      list(
+        bottom.padding = list(x = bottom.padding),
+        top.padding = list(x = top.padding)
+    ),
+    layout.widths =
+      list(
+        left.padding = list(x = left.padding),
+        right.padding = list(x = right.padding)
+      ),
+    axis.padding = list(numeric = axis.padding)
+  )
+
+}
 
 
 
-
-
-
-
+# 
+#  
+# 
+# plot_allEffects(fit,
+#                 labels = get_label(mtcars2)
+# )
+# 
+#  lattice::lattice.getOption("axis.padding")
